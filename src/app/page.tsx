c2ddf98697dc62fb253253b616c6b781513a4db6
Grecco, Franco <srgrecco93@gmail.com>
2025-04-25 17:43:23 +0200

    feat: add desktop UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,26 @@ export default function Home() {
   } = useApp();
 
   return (
-    <div className="flex flex-col h-screen items-center bg-background/40">
+    <div
+      className={cn(
+        "flex flex-col h-screen items-center bg-background/40",
+        "sm: justify-center"
+      )}
+    >
       <img src={"/logo.svg"} alt="Splitter logo" className="m-12" />
-      <div className="flex flex-col w-full h-full gap-8 px-6 py-8 rounded-t-2xl bg-surface">
+      <div
+        className={cn(
+          "flex flex-col w-full h-fit gap-8 px-6 py-8 rounded-t-2xl bg-surface",
+          "sm:flex-row sm:max-w-3xl sm:rounded-2xl"
+        )}
+      >
         {/* Settings */}
-        <div className="flex flex-col gap-8">
+        <div
+          className={cn(
+            "flex flex-col gap-8",
+            "sm:justify-between sm:flex-1/2"
+          )}
+        >
           {/* Bill Amount */}
           <Input
             id="bill-amount"
@@ -41,7 +56,7 @@ export default function Home() {
           {/* Tip Selection */}
           <div className="flex flex-col w-full gap-1">
             <h2>Select Tip %</h2>
-            <div className="grid grid-cols-2 gap-2">
+            <div className={cn("grid grid-cols-2 gap-2", "sm:grid-cols-3")}>
               {["5", "10", "15", "25", "50"].map((item) => (
                 <TipButton
                   key={item}
@@ -79,63 +94,67 @@ export default function Home() {
             error={hasPeopleError}
             errorMessage="Can't be zero"
             onChange={(e) => actions.updatePeople(e.target.value)}
-            onFocusCapture={() => actions.updatePeople("1")}
             placeholder={0}
           />
+        </div>
 
-          {/* Display */}
-          <div className="flex flex-col flex-grow justify-between gap-8 p-4 rounded-xl bg-primary-darker text-white">
-            <div className="flex flex-col flex-grow gap-4">
-              <div className="flex justify-between items-center">
-                <div className="flex flex-col gap-1">
-                  <h2 className="font-semibold">Tip Amount</h2>
-                  <h3 className="text-primary-text-dimmed font-semibold">
-                    / person
-                  </h3>
-                </div>
-
-                <div className="flex justify-end items-center gap-1">
-                  <img
-                    src={"/icon-dollar.svg"}
-                    alt="Dollar icon"
-                    className="text-primary"
-                  />
-                  <h2 className="text-xl text-primary">
-                    {helpers.getTipPerPerson()}
-                  </h2>
-                </div>
+        {/* Display */}
+        <div
+          className={cn(
+            "flex flex-col flex-grow justify-between gap-8 p-4 rounded-xl bg-primary-darker text-white",
+            "sm:flex-1/2"
+          )}
+        >
+          <div className="flex flex-col flex-grow gap-4">
+            <div className="flex justify-between items-center">
+              <div className="flex flex-col gap-1">
+                <h2 className="font-semibold">Tip Amount</h2>
+                <h3 className="text-primary-text-dimmed font-semibold">
+                  / person
+                </h3>
               </div>
-              <div className="flex justify-between items-center">
-                <div className="flex flex-col gap-1">
-                  <h2 className="font-semibold">Total</h2>
-                  <h3 className="text-primary-text-dimmed font-semibold">
-                    / person
-                  </h3>
-                </div>
 
-                <div className="flex justify-end items-center gap-1">
-                  <img
-                    src={"/icon-dollar.svg"}
-                    alt="Dollar icon"
-                    className="text-primary"
-                  />
-                  <h2 className="text-xl text-primary">
-                    {helpers.getTotalPerPerson()}
-                  </h2>
-                </div>
+              <div className="flex justify-end items-center gap-1">
+                <img
+                  src={"/icon-dollar.svg"}
+                  alt="Dollar icon"
+                  className="text-primary"
+                />
+                <h2 className="text-xl text-primary">
+                  {helpers.getTipPerPerson()}
+                </h2>
               </div>
             </div>
-            <button
-              onClick={() => actions.reset()}
-              className={cn(
-                "p-2 rounded-lg bg-primary-accent",
-                "hover:cursor-pointer hover:bg-background/40",
-                "text-primary-accent-text uppercase"
-              )}
-            >
-              Reset
-            </button>
+            <div className="flex justify-between items-center">
+              <div className="flex flex-col gap-1">
+                <h2 className="font-semibold">Total</h2>
+                <h3 className="text-primary-text-dimmed font-semibold">
+                  / person
+                </h3>
+              </div>
+
+              <div className="flex justify-end items-center gap-1">
+                <img
+                  src={"/icon-dollar.svg"}
+                  alt="Dollar icon"
+                  className="text-primary"
+                />
+                <h2 className="text-xl text-primary">
+                  {helpers.getTotalPerPerson()}
+                </h2>
+              </div>
+            </div>
           </div>
+          <button
+            onClick={() => actions.reset()}
+            className={cn(
+              "p-2 rounded-lg bg-primary-accent",
+              "hover:cursor-pointer hover:bg-teal-300",
+              "text-primary-accent-text font-bold uppercase"
+            )}
+          >
+            Reset
+          </button>
         </div>
       </div>
     </div>
